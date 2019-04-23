e83b74de4959df1ead536db337d54a154325073e
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-23 23:44:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+
+class School
+  def initialize(school_name, roster)
+    @school_name = school_name
+    @roster = roster
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  # def add_student(student,grade)
+  #     @roster[grade] ||= []
+  #     @roster[grade] << student
+  # end
+  
+  def grade(grade)
+    
+  end
+  
+  def sort
+    @roster.sort
+  end
+end
